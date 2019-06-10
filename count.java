6856e2a32a000d6642ffe595451f28104e15274c
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-10 16:07:06 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int count=0;
+        int n=scan.nextInt();
+        int a[]=new int[n];
+        for(;n!=0;n/=10,count++){
+            
+        }
+        System.out.println(count);
+}
+
+}

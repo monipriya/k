70ecc78da9975b9f537819bd5fdaf74eebcdf59c
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-10 11:20:32 +0530

    Update and rename vowel.java to v.java

---
+++ v.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+char s=sc.next().charAt(0);
+if(s=='a'|| s=='e'|| s=='i' ||s=='o' ||s=='u'){
+System.out.println("Alphabet");
+}
+else{
+System.out.println("no");
+}
+}
+}

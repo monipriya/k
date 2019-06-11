cf9bb8598516e676d64f86628973f774f3798d2e
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-11 17:42:24 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class even {
+
+public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		for(int i=n;i<k;i++){
+			if(i%2==0 && i!=n){
+				System.out.println(i);
+			}
+				
+		}
+	}
+	}

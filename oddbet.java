d67d35ba5212b0b2ea08478b54bf847bee0d1a2d
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-11 17:19:22 +0530

    Create oddbet.java

---
+++ oddbet.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class oddbetween {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int k=sc.nextInt();
+	for(int i=n;i<=k;i++){
+		if(i%2!=0){
+			System.out.println(i);
+		}
+		
+	}
+}
+}

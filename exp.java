2019ebb079ff2c0454eed74d014ed18baa77d78a
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-11 14:20:34 +0530

    Create exp.java

---
+++ exp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int base=scan.nextInt();
+		int exp=scan.nextInt();
+		int result=Math.pow(base,exp);
+	    System.out.println(result);
+	}
+}

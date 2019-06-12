baa2793f10c3b162b7adb4f93a8e50a651c02295
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-12 14:31:14 +0530

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class multiple {
+public static void main(String[] args) {
+	Scanner scan= new Scanner(System.in);
+	int n=scan.nextInt();
+	//int k=scan.nextInt();
+	for(int i=1;i<=5;i++){
+		System.out.println(i*n);
+}
+}
+
+}

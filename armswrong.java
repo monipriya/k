7882dbd34632f3a1d8f28a15c55b9e87f24d7c0a
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-12 17:28:20 +0530

    Create armswrong.java

---
+++ armswrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class armsbet {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		for(int i = n1;i<n2;i++){
+		int r =0,p=i;
+		int s = 0;
+		while(p >0){
+		r = p % 10;
+		p = p / 10;
+		s = s + (r * r * r);
+		}
+		if(s == i){
+		System.out.println(i);
+		}
+		}
+
+		}
+		}

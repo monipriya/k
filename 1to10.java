26d8c9851a7a6f1a5dd0873441a017156e57d151
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-13 11:24:34 +0530

    Create 1to10.java

---
+++ 1to10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class string {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	//int str=s.nextInt();
+	int n=s.nextInt();
+
+		if(n<=10 && n>0){
+			System.out.println("yes");
+		}
+		else{
+		System.out.println("no");
+	}
+	}		
+}

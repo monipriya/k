7c3b9f563a090d5e05483b787f8da619637f3c0d
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-13 12:04:27 +0530

    Create c1.java

---
+++ c1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class cnext {
+public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt();
+	int p=scan.nextInt();
+		System.out.println(n+p);
+	
+}
+}

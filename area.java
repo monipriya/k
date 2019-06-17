eca5f43959c2cc7e33466edc25884dd2fdc4dc6e
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-17 17:52:05 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class area {
+ public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	int n=scan.nextInt();
+	int h=scan.nextInt();
+	int a;
+	System.out.println(a=((h*n)/2));
+}
+}

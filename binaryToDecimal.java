cc493e4ca3e31a811e23333156fdba8254110c25
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-17 15:18:13 +0530

    Create binaryToDecimal.java

---
+++ binaryToDecimal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class decimaltobinary {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    String str=String.valueOf(n);
+	System.out.println(Integer.parseInt(str,2));
+}
+}

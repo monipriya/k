c015280e7476b3bc9d00b6137d054fdf2a516ec3
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-17 10:24:58 +0530

    Update fac1.java

--- fac1.java
+++ fac1.java
@@ -4,7 +4,7 @@ public static void main(String[] args){
 Scanner s=new Scanner(System.in);
 	int r=s.nextInt();
 	int f=1;
-	for(int i=0;i<=n;i++){
+	for(int i=1;i<=n;i++){
 		f=f*i;
 	}
 	System.out.println(f);

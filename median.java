799c9214d8c16d1a73d2fa470274a57cee8d80e6
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-20 17:55:42 +0530

    Create median.java

---
+++ median.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxminset2 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++){
+		a[i]=sc.nextInt();
+			int m=((n-1)/2);
+			System.out.println(a[m-1]);
+	   break;	
+	}
+
+}}
+        

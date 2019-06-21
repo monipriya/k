50094f6d71065bda864fbe741306b8186c018f51
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-21 14:06:13 +0530

    Create sumdigit.java

---
+++ sumdigit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class defaultp {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int r,sum=0;
+	while(n>0){
+		r=n%10;
+		sum=sum+r;
+		n/=10;
+	}
+	System.out.println(sum);
+}
+}

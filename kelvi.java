2c7826bee2141e7af65b945ec640c623a649e5f6
monipriya <47966750+monipriya@users.noreply.github.com>
2019-06-28 17:45:10 +0530

    Create kelvi.java

---
+++ kelvi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class product {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+double t=(n+273.75);
+System.out.println((int)Math.floor(t));
+	}
+}

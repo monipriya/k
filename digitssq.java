3e067c253f78b3a7124b95f71aa57c2295b99445
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-08 14:37:28 +0530

    Rename digitsquar.java to digitssq.java

---
+++ digitssq.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.*
+;public class countwords {
+	public static void main(String[] args) {
+	 	Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int r,sum=0;
+        while(n!=0){
+        	r=n%10;
+        	sum=sum+r*r;
+        	n=n/10;
+        
+        }
+        System.out.println(sum);
+	}
+}

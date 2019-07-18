07e498c5395ebb587c95c7f1138f899887824c42
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-18 15:41:13 +0530

    Create zeroodd.java

---
+++ zeroodd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class beginerset7 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+int n1=sc.nextInt();
+int c=(int)Math.abs(n-n1);
+
+
+{if(c>0){
+if(c%2==0){
+	
+	System.out.println("even");
+}
+System.out.println("odd");
+}
+else{
+	System.out.println("even");
+}
+}
+}
+}

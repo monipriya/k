1745f370d7b1d1bfe6bdecb67715f12f75da6c37
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-18 14:32:16 +0530

    Create subset.java

---
+++ subset.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class beginerset7 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String s=sc.next();
+	int n=sc.nextInt();
+	System.out.println(s.substring(0,n));
+			}
+}

8a457a63eb4a44fa35ad145e4e44deed92e1a4e4
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-22 14:03:55 +0530

    Update guviplayerset4String.java

--- guviplayerset4String.java
+++ guviplayerset4String.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	String s=sc.nextLine();
 	String str="";
-	for(int i=0;i<s.length()-1;i+=3){
+	for(int i=0;i<=s.length()-1;i+=3){
 		str+=s.charAt(i);
 	}
 	System.out.println(str);

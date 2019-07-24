4988d8102a58aa321bd5dafda1e993420d83c230
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-24 17:53:33 +0530

    Create Guviset7Append.java

---
+++ Guviset7Append.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Guviset7Append {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	String res=str+" "+"Answer";
+	System.out.println(res);
+}
+}

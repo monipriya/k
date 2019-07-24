28977c2ec29d0988fbc10f87b803b2e69023a49b
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-24 19:32:35 +0530

    Update guvi8arraymerge.java

--- guvi8arraymerge.java
+++ guvi8arraymerge.java
@@ -4,8 +4,9 @@ public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter the array size");
 	int a=sc.nextInt();
+	int b=sc.nextInt();
 	int arr[]=new int[a];
-	int brr[]=new int[a];
+	int brr[]=new int[b];
 	ArrayList<Integer> arr1=new ArrayList<Integer>();
 
 	for(int i = 0;i<a;i++)
@@ -15,7 +16,7 @@ public static void main(String[] args) {
 
 	}
 	
-	for(int i = 0;i<arr.length;i++)
+	for(int i = 0;i<b;i++)
 	{
 		brr[i] = sc.nextInt();
 		arr1.add(brr[i]);

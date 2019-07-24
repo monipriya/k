c219f5f487a23eef48836000edad1b6fff345372
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-24 18:11:21 +0530

    Create Guviset8pow.java

---
+++ Guviset8pow.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Guviset8pow {
+	public static void main(String[] args) {
+		
+	
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+System.out.println((int)Math.pow(a, b));
+}
+}

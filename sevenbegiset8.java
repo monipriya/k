1a0b652c18de1699d6c14d822946385cb45e7c78
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-26 19:06:46 +0530

    Create sevenbegiset8.java

---
+++ sevenbegiset8.java
@@ -0,0 +1,16 @@
+import java.util.*;	
+public class sevenbegiset8 {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+		if(n%13==0){
+			System.out.println("yes");
+		
+		}
+		else{
+			System.out.println("no");
+		
+		}
+	}
+}
+

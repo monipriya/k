6df7761fc90cb579b1e8bd27a56ce0ec1adf38a0
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-26 19:59:28 +0530

    Create guviset6len.java

---
+++ guviset6len.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class guviset6len {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str=sc.nextLine();
+	char[] ch=str.toCharArray();
+	int c=0;
+	for(int i=0;i<ch.length;i++){
+		c++;
+	}
+	System.out.println(c);
+}
+}

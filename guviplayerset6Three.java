605ec53780699fd3f2ae13580891f008f3862298
monipriya <47966750+monipriya@users.noreply.github.com>
2019-07-26 19:24:56 +0530

    Create guviplayerset6Three.java

---
+++ guviplayerset6Three.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class guviplayerset6Three {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String str=sc.next();
+	String st=sc.next();
+	if(str.contains(st)){
+		System.out.println("yes");
+	}
+	else{
+		System.out.println("no");
+	}
+}
+}
